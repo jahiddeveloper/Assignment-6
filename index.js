580e2333f5544d2641a38af7d71a72dfb7f3e2bb
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-09-08 17:36:37 +0600

    Cart compleat

--- index.js
+++ index.js
@@ -17,4 +17,57 @@ const displayCategories = (categories) => {
     categoriesContainer.append(btnDiv);
   }
 };
+
 loadCategories();
+
+const loadCart = () => {
+  fetch("https://openapi.programming-hero.com/api/plants")
+    .then((res) => res.json())
+    .then((json) => displayCart(json.plants));
+};
+
+//       {
+    //   "id": 1,
+    //   "image": "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg",
+    //   "name": "Mango Tree",
+    //   "description": "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals.",
+    //   "category": "Fruit Tree",
+    //   "price": 500
+    // },
+
+const displayCart = (carts) => {
+  const cartContainer = document.getElementById("cart-container");
+  cartContainer.innerHTML = "";
+
+  for (const cart of carts) {
+    const cartDiv = document.createElement("div");
+    cartDiv.innerHTML = `
+    <div class="w-90 bg-white shadow-lg p-4 rounded-xl">
+          <div class="w-full h-60">
+            <img
+              src="${cart.image}"
+              alt="Mango"
+              class="w-full h-full object-cover object-center rounded-lg"
+            />
+          </div>
+
+          <h2 class="font-bold text-lg mt-3 mb-2">${cart.name}</h2>
+          <p class="text-[#1f2937] text-justify">
+            ${cart.description}
+          </p>
+          <div class="flex justify-between items-center mt-2">
+            <span
+              class="btn text-[#15803d] bg-[#facc15] rounded-3xl px-5 font-semibold border-none hover:bg-[#fcd435] hover:scale-105 transition transform duration-300"
+              >${cart.category}</span
+            >
+            <p class="font-bold">৳<span>${cart.price}</span></p>
+          </div>
+          <button class="btn mt-4 w-full text-white bg-[#15803d] hover:bg-[#27af59] border-none rounded-3xl">Success</button>
+        </div>
+    `
+
+    cartContainer.append(cartDiv)
+  }
+};
+
+loadCart();
